2bc71c334f00ef4a7955b99d4b30e3fe6629c36a
sangharsh <sangharsh_iitd@yahoo.com>
2025-10-22 10:54:21 +0100

    failsafe plugin for IT added

---
+++ src/test/java/com/example/junit5/SimpleIT.java
@@ -0,0 +1,13 @@
+package com.example.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleIT {
+
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
