f45d28609934f7a14da58d5e98fd9fc22beb83bd
sangharsh <sangharsh_iitd@yahoo.com>
2025-10-22 11:06:11 +0100

    jmeter dsl with a Test added

---
+++ src/test/java/com/example/junit5/PerformanceTest.java
@@ -0,0 +1,24 @@
+package com.example.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import java.io.IOException;
+import java.time.Duration;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+@Execution(ExecutionMode.SAME_THREAD)
+public class PerformanceTest {
+
+    @Test
+    public void testPerformance() throws IOException {
+        testPlan(
+                threadGroup(10, Duration.ofSeconds(60),
+                        httpSampler("https://jmeter.apache.org/")
+                ),
+                // This will generate an HTML report with graphs and stats
+                htmlReporter("target/jmeter-report")
+        ).run();
+    }
+}
